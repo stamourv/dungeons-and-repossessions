54a2cf8ded2269c3d562fffbe35342290c545cfa
Vincent St-Amour <stamourv@racket-lang.org>
2016-01-03 09:45:03 -0600

    Map -> Grid
    A Map will be with additional info, like the player, etc.

--- grid.rkt
+++ grid.rkt
@@ -4,11 +4,11 @@
          math/array math/matrix
          "cell.rkt")
 
-;; a Map is a math/matrix Matrix of cell%
+;; a Grid is a math/matrix Matrix of cell%
 
-;; parses a list of strings into a map, based on the printed representation
+;; parses a list of strings into a grid, based on the printed representation
 ;; of each cell
-(define (parse-map los)
+(define (parse-grid los)
   (for*/matrix (length los)
                (apply max (map string-length los))
                #:fill (new cell%)
@@ -16,10 +16,10 @@
                 [c (in-string s)])
      (new (char->cell% c))))
 
-(define (show-map m)
+(define (show-grid g)
   (with-output-to-string
     (lambda ()
-      (for ([r (in-list (matrix-rows m))])
+      (for ([r (in-list (matrix-rows g))])
         (for ([c (in-array r)])
           (display (send c show)))
         (newline)))))
@@ -28,26 +28,26 @@
 (module+ test
   (require rackunit)
 
-  (define (parse-and-show los) (show-map (parse-map los)))
-  (define (render-map m) (string-join m "\n" #:after-last "\n"))
+  (define (parse-and-show los) (show-grid (parse-grid los)))
+  (define (render-grid g) (string-join g "\n" #:after-last "\n"))
 
-  (define m1
+  (define g1
     '(" "))
-  (check-equal? (parse-and-show m1) " \n")
+  (check-equal? (parse-and-show g1) " \n")
 
-  (define m2
+  (define g2
     '("**********"
       "*        *"
       "*        *"
       "*        *"
       "**********"))
-  (check-equal? (parse-and-show m2) (render-map m2))
+  (check-equal? (parse-and-show g2) (render-grid g2))
 
-  (define m3 ; padding should work
+  (define g3 ; padding should work
     '("**********"
       "*        *"
       "*        *"
       "*        *"
       "*****"))
-  (check-equal? (parse-and-show m3) (render-map m2))
+  (check-equal? (parse-and-show g3) (render-grid g2))
   )
