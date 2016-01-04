b75cd1f8eb4a58ba4443ce6e49fdd1f6943f0023
Vincent St-Amour <stamourv@racket-lang.org>
2016-01-03 20:26:20 -0600

    Cleanup.

--- game.rkt
+++ game.rkt
@@ -24,19 +24,16 @@
 (define (game-loop s)
   (define active-character (first (state-initiative-order s)))
   (define action-taken
-    (cond [(equal? active-character
-                   (state-player s))
-           ;; TODO should this be a player's `act` method?
+    (cond [(equal? active-character (state-player s))
            (display-state s)
            (handle-input s)]
-          [else
+          [else ; npc
            (send active-character act (state-mode s))]))
   (unless (equal? action-taken 'quit)
     (define new-s (state-cleanup (next-state s action-taken)))
     (cond [(positive? (get-field current-hp (state-player new-s)))
            (game-loop new-s)] ; alive, keep going
           [else
-           ;; TODO handle this in a player `die` method?
            (printf "~a has died.\nGame over.\n"
                    (send (state-player new-s) describe
                          #:capitalize? #t))])))
