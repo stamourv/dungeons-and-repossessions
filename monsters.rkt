cb5e82cefcd6ac886acc3c93c436a4a482855ae5
Vincent St-Amour <stamourv@racket-lang.org>
2016-01-04 21:13:08 -0600

    More monsters.

--- monsters.rkt
+++ monsters.rkt
@@ -17,12 +17,27 @@
         char)
       ;; TODO act method
       (super-new [name n] [max-hp hp] [xp-value xp]))))
+;; TODO add ac, damage die, stats, speed, etc.
 
 (define-simple-monster bat% #\b "bat" 1 10) ; TODO 1 -> (max 1d4-1 1)
 (make-encounter 1 'easy (new bat%))
+(make-encounter 1 'hard (new bat%) (new bat%))
 
 (define-simple-monster rat% #\r "rat" 1 10) ; TODO 1 -> (max 1d4-1 1)
 (make-encounter 1 'easy (new rat%))
+(make-encounter 1 'hard (new rat%) (new rat%))
+(make-encounter 1 'deadly (new rat%) (new rat%) (new rat%))
+
+(define-simple-monster spider% #\s "spider" 1 10) ; TODO 1 -> 1d-1
+;; TODO should ignore difficult terrain
+
+(define-simple-monster giant-rat% #\R "giant rat" 7 25) ; TODO 7 -> 2d6
+(make-encounter 1 'medium (new giant-rat%))
+(make-encounter 1 'deadly (new giant-rat%) (new rat%))
+
+(define-simple-monster kobold% #\k "kobold" 5 25) ; TODO 5 -> 2d6 - 2
+(make-encounter 1 'medium (new kobold%))
+(make-encounter 1 'deadly (new kobold%) (new spider%))
 
 
 ;; TODO other monsters. see page 61 of DM Basic Rules for monster per CR
