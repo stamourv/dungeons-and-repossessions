d3bc07ff411007c0830fa1ae15a850fbf2c312d8
Vincent St-Amour <stamourv@racket-lang.org>
2016-01-04 21:04:35 -0600

    Shortcut for defining monsters.

--- monsters.rkt
+++ monsters.rkt
@@ -10,24 +10,18 @@
     [init-field xp-value]
     (super-new)))
 
-(define bat%
-  (class monster%
-    (define/override (show)
-      #\b)
-    ;; TODO act method
-    (super-new [name     "bat"]
-               [max-hp   1] ;; TODO (max 1d4-1 1)
-               [xp-value 10])))
+(define-syntax-rule (define-simple-monster def-name char n hp xp)
+  (define def-name
+    (class monster%
+      (define/override (show)
+        char)
+      ;; TODO act method
+      (super-new [name n] [max-hp hp] [xp-value xp]))))
+
+(define-simple-monster bat% #\b "bat" 1 10) ; TODO 1 -> (max 1d4-1 1)
 (make-encounter 1 'easy (new bat%))
 
-(define rat%
-  (class monster%
-    (define/override (show)
-      #\r)
-    ;; TODO act method
-    (super-new [name     "rat"]
-               [max-hp   1] ;; TODO (max 1d4-1 1)
-               [xp-value 10])))
+(define-simple-monster rat% #\r "rat" 1 10) ; TODO 1 -> (max 1d4-1 1)
 (make-encounter 1 'easy (new rat%))
 
 
