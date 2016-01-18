7dda7a4c2465128bcc1cfd04c9aec42f4eb251d3
Vincent St-Amour <stamourv@racket-lang.org>
2016-01-17 22:16:37 -0600

    Additional link.

--- vision.rkt
+++ vision.rkt
@@ -8,6 +8,7 @@
 
 ;; shadow casting
 ;; based on: http://www.roguebasin.com/index.php?title=Improved_Shadowcasting_in_Java
+;; algorithm: http://www.roguebasin.com/index.php?title=FOV_using_recursive_shadowcasting
 (define (compute-fov grid pos radius)
   (match-define (vector start-x start-y) pos)
   (define fov (mutable-set pos))
